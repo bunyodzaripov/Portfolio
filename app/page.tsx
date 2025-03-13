483b297ad0427f2f790f96728bb1d1d6f184404d
bunyodzaripov <zaripovbunyod571@gmail.com>
2025-03-13 13:08:00 +0500

    added about

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Navbar from "../app/components/navbar";
 import Header from "../app/components/header";
+import About from "../app/components/about";
 
 export default function Home() {
    return (
       <>
          <Navbar />
          <Header />
+         <About />
       </>
    );
 }
