f9ba42a3a8bd9fc652840bac7e63d4ef05c9ef3c
bunyodzaripov <zaripovbunyod571@gmail.com>
2025-03-17 13:45:22 +0500

    optimaze

---
+++ app/components/index.ts
@@ -0,0 +1,9 @@
+import About from "./about";
+import Contact from "./contact";
+import Footer from "./footer";
+import Header from "./header";
+import Navbar from "./navbar";
+import Service from "./service";
+import Work from "./work";
+
+export { About, Contact, Footer, Header, Navbar, Service, Work };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Navbar from "../app/components/navbar";
-import Header from "../app/components/header";
-import About from "../app/components/about";
-import Service from "../app/components/service";
-import Work from "../app/components/work";
-import Contact from "@/app/components/contact";
-import Footer from "@/app/components/footer";
+import {
+   About,
+   Contact,
+   Footer,
+   Header,
+   Navbar,
+   Service,
+   Work,
+} from "./components";
 
 export default function Home() {
    return (
